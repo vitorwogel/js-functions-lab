9abb64b120fc9d72bc8263bb877fd9ebd5104d6a
Vitor Andre Brochado Wogel da Costa <vitorandre26@gmail.com>
2022-09-22 12:07:32 +0100

    exercise 1

---
+++ exercise01.js
@@ -0,0 +1,15 @@
+fetch("https://jsonplaceholder.typicode.com/todos")
+  .then(response => response.json())
+  .then(json => {
+
+    const completed = json.filter(
+        json => json.completed === true
+    );
+
+    completed.forEach( (todo, index) => {
+        console.log(`${todo.title} - ${todo.completed}`)
+    })
+  })
+  .catch(function(err) { 
+    console.log(err);
+  });
